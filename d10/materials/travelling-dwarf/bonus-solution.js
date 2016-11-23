d151d723198641c89f8ac96751be6d9111e89583
mpolhamus <missypolhamus@gmail.com>
2016-11-22 20:14:42 -0500

    Week 5 Home work

---
+++ d10/materials/travelling-dwarf/bonus-solution.js
@@ -0,0 +1,6 @@
+
+var x = 0;
+
+while ( g.at(x, y) != 0){
+d.move ();
+}
---
+++ d11/materials/queryland/js/my-code.js
@@ -0,0 +1,8 @@
+$("button").on("click", function() {
+  var title = $("input").val();
+  $("h1").text(title);
+});
+
+$("p").css("color", "red");
+
+$("p.awesome").css("background-color", "purple");
